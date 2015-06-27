336524ed35b76811fc46055424d30d4f6dc1cfa4
Gabriel Caires <gabrielcairesmar@gmail.com>
2015-06-27 08:59:28 -0300

    Show identation mark

--- modules/ui.el
+++ modules/ui.el
@@ -1,4 +1,16 @@
 (use-package powerline
+  :config
+  (powerline-raw mode-line-mule-info nil 'l)
   :init
   (powerline-default-theme))
+
+(use-package highlight-indentation
+  :diminish highlight-indentation-mode
+  :defer 2
+  :config
+  (highlight-indentation-mode 1)
+  )
+
+
+
 (provide 'ui)
