cb093f643798929ee3664f99f0b717c80a7a9bbf
Gabriel Caires <gabrielcairesmar@gmail.com>
2015-06-27 00:33:45 -0300

    Clean some old configsa

--- core/editor.el
+++ core/editor.el
@@ -7,7 +7,24 @@
 (toggle-scroll-bar -1)
 (menu-bar-mode -1)
 
+;; mode line settings
+(line-number-mode t)
+(column-number-mode t)
+(size-indication-mode t)
+
 ;; Initial Startup
+(setq inhibit-startup-message t)
+
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier 'super)
+(setq ns-function-modifier 'hyper)
+
+;; Turn off mouse interface early in startup to avoid momentary display
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; No splash screen please ... jeez
 (setq inhibit-startup-message t)
 
 ;; Tab configurations
@@ -18,7 +35,32 @@
 ;; Remove white space
 (add-hook 'before-save-hook 'whitespace-cleanup)
 
+;; enable y/n answers
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; Newline at end of file
+(setq require-final-newline t)
+
+;; delete the selection with a keypress
+(delete-selection-mode t)
+
+;; store all backup and autosave files in the tmp dir
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+;; autosave the undo-tree history
+(setq undo-tree-history-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq undo-tree-auto-save-history t)
+
 ;; Theme
 (load-theme 'zenburn t)
 
+;; disable annoying blink-matching-paren
+(setq blink-matching-paren nil)
+
+(global-hl-line-mode 1)
+(set-face-background 'hl-line "#330")
 (provide 'editor)
